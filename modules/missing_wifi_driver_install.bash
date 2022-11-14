fcd6e9e4ff7e32f0e55a0ddfea1119ceb817185c
alex <alex@solidstateofmind.com>
2022-11-14 16:26:08 -0500

    improve machine matching

--- modules/missing_wifi_driver_install.bash
+++ modules/missing_wifi_driver_install.bash
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
-machine=$(uname -n)
-if [[ $machine == x17-R2* ]]; then
+# Install dependency
+sudo apt install -y neofetch
+# Check if the machine is an Alienware x17 R2
+machine=$(neofetch model)
+if [[ $machine == *Alienware\ x17\ R2* ]]; then
     echo "INSTALLING: Wi-Fi driver backport. Alienware x17 R2 model detected"
     wget https://launchpad.net/ubuntu/+source/backport-iwlwifi-dkms/9858-0ubuntu3/+build/23560630/+files/backport-iwlwifi-dkms_9858-0ubuntu3_all.deb
     sudo dpkg -i backport-iwlwifi-dkms_*.deb
