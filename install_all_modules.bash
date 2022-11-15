7d2f5dcfff9481e81ea1aeb478c5593798cba0fa
alex <alex@solidstateofmind.com>
2022-11-15 14:10:30 -0500

    added comments on the modules utility

--- install_all_modules.bash
+++ install_all_modules.bash
@@ -1,15 +1,28 @@
 #!/usr/bin/env bash
 sudo apt update && sudo apt upgrade -y
-sudo apt install -y git git-lfs vim htop gcc-10 openssh-server curl apt-utils neofetch
+sudo apt install -y apt-utils curl htop neofetch vim
 cd modules/
+
+# Drivers
 ./nvidia_driver_install.bash
 ./missing_wifi_driver_install.bash
-./github_cli_install.bash
-./speedtest_cli_install.bash
-./ros_install.bash
-./vscode_install.bash
+
+# Software
 ./google_chrome_install.bash
 ./slack_install.bash
+
+# Generic tools
+sudo apt install -y openssh-server
+./speedtest_cli_install.bash
 ./obs_studio_install.bash
+
+# Developer tools
+sudo apt install -y git git-lfs gcc-10
+./github_cli_install.bash
+./vscode_install.bash
+
+# Robotics specific
+./ros_install.bash
 ./gazebo_fortress_install.bash
+
 cd ../
