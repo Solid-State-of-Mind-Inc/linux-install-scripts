c0396be4ca344fadf2e480db5381e50024d95728
alex <alex@solidstateofmind.com>
2022-11-17 10:05:30 -0500

    add script to install flameshot

---
+++ modules/flameshot_install.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+OWNER="flameshot-org"
+REPO="flameshot"
+LATEST_RELEASE=$(curl -L -s -H 'Accept: application/json' https://github.com/$OWNER/$REPO/releases/latest/)
+LATEST_VERSION=$(echo $LATEST_RELEASE | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
+LATEST_VERSION_NUMBER=$(echo $LATEST_VERSION | sed 's/v//')
+echo "flameshot latest release version: $LATEST_VERSION"
+echo "LATEST_VERSION_NUMBER: $LATEST_VERSION_NUMBER"
+wget https://github.com/$OWNER/$REPO/releases/download/$LATEST_VERSION/flameshot-$LATEST_VERSION_NUMBER-1.debian-10.amd64.deb
+sudo dpkg -i flameshot-*.deb
+rm flameshot-*.deb
